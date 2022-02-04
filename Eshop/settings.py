9e60a7d9d2ba5ae83a8a868211d15a916a899427
latototita <85679433+latototita@users.noreply.github.com>
2022-02-04 03:45:09 -0500

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-_from_env = dj_database_url.config(conn_max_age=600)
+db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
 # Password validation
