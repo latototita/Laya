ad70f8c02d6aa8ae12779e1a99c377183e7a2c31
latototita <85679433+latototita@users.noreply.github.com>
2022-02-04 04:55:13 -0500

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['*','https://pearlmarto.herokuapp.com','localhost']
 
 INSTALLED_APPS = [
     'admin_interface',
-    'django-environ',
     'whitenoise.runserver_nostatic',
     'colorfield',
     'django.contrib.admin',
