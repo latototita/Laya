25305883a884014c67d9cc5f318eb93be3cf2101
latototita <85679433+latototita@users.noreply.github.com>
2022-02-04 04:30:14 -0500

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -81,8 +81,10 @@ WSGI_APPLICATION = 'Eshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 DATABASES = {
-    # read os.environ['DATABASE_URL'] and raises ImproperlyConfigured exception if not found
-    'default': env.db(),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 db_from_env = dj_database_url.config(conn_max_age=600)
