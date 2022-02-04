52316daef84b67f781ae20181bba43afdd300981
latototita <85679433+latototita@users.noreply.github.com>
2022-02-04 04:32:51 -0500

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -83,9 +83,10 @@ WSGI_APPLICATION = 'Eshop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
